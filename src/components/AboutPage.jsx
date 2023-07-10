75246a7b84bb28cbd706274e7e5d947522e73a41
patelrudy <100442399+patelrudy@users.noreply.github.com>
2023-07-10 14:03:04 -0400

    fixed typing animation error

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,34 +1,67 @@
-import React from 'react';
-import useTypewriter from 'react-typewriter-hook';
+import React, { useEffect, useState } from 'react';
 import PlexusBackground from './PlexusBackground';
 import { Link } from 'react-router-dom';
 import './AboutPage.css';
 import personal_image from '../assets/personal_image.webp'
 
-const MagicOcean = [
+const words = [
   'Analytical', 'Creative', 'Adaptable', 'User-Focused'
 ];
 
-let index = 0;
+const typingDelay = 100; // delay between typing each letter
+const erasingDelay = 50; // delay between erasing each letter
+const pauseDelay = 2000; // pause before deleting word and moving to the next one
 
 const TypewriterEffect = () => {
-  const [magicName, setMagicName] = React.useState(MagicOcean[index]);
-  const intervalRef = React.useRef({});
-  const name = useTypewriter(magicName);
+  const [wordIndex, setWordIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [reverse, setReverse] = useState(false);
+  const [blink, setBlink] = useState(true);
+  const [typing, setTyping] = useState(false);
 
-  React.useEffect(() => {
-    intervalRef.current = setInterval(() => {
-      index = (index + 1) % MagicOcean.length;
-      setMagicName(MagicOcean[index]);
-    }, 5000);
+  // blinking cursor
+  useEffect(() => {
+    let cursor;
+    if (!typing) {
+      cursor = setInterval(() => {
+        setBlink((prevBlink) => !prevBlink);
+      }, 500);
+    }
+    return () => clearInterval(cursor);
+  }, [typing]);
 
-    return function clear() {
-      clearInterval(intervalRef.current);
-    };
-  }, [magicName]);
+  // typewriter
+  useEffect(() => {
+    if(subIndex === words[wordIndex].length + 1 && !reverse) {
+      setTyping(false);
+      setTimeout(() => {
+        setReverse(true);
+        setTyping(true);
+      }, pauseDelay);
+    } else if (!reverse && subIndex <= words[wordIndex].length) {
+      setTyping(true);
+      setTimeout(() => {
+        setSubIndex((prevSubIndex) => prevSubIndex + 1);
+      }, typingDelay);
+    } else if (reverse && subIndex > 0) {
+      setTyping(true);
+      setTimeout(() => {
+        setSubIndex((prevSubIndex) => prevSubIndex - 1);
+      }, erasingDelay);
+    } else if (subIndex === 0 && reverse) {
+      setTyping(false);
+      setReverse(false);
+      setWordIndex((prevWordIndex) => (prevWordIndex + 1) % words.length);
+    }
+  }, [subIndex, wordIndex, reverse]);
 
-  return <h2>{name}</h2>;
-}
+  return (
+    <h2>
+      {`${words[wordIndex].substring(0, subIndex)}${typing || blink ? "|" : "\u00a0"}`}
+    </h2>
+  );
+};
+
 
 const AboutPage = () => {
   return (
